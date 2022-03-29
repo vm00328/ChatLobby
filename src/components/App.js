399c636a705822b0490f6a9d4c6c8e57940b03a3
Vladislav Manolov <vm00328@surrey.ac.uk>
2022-03-29 23:34:55 +0100

    Initial Commit with error when rendering web page

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+ import React from "react"
+ import Signup from "./Signup"
+
+ function App() {
+   // Error is here
+   return <Signup />
+ }
+//  require('react-dom');
+// window.React2 = require('react');
+// console.log(window.React1 === window.React2);
+
+ // this.setState({something: 'changed'});
+ export default App
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+// window.React1 = require('react');
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
